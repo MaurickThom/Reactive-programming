9df2ecdf2c5908064d7b4766085ac68b5e281eb4
MaurickThom <thomtwd@gmail.com>
2020-03-14 00:48:45 -0500

    status bar rxjs

--- Rxjs-desde-cero-hasta-los-detalles/ts-webpack/webpack-workflow/src/ts/status-bar.ts
+++ Rxjs-desde-cero-hasta-los-detalles/ts-webpack/webpack-workflow/src/ts/status-bar.ts
@@ -1,1 +1,22 @@
-import {} from 'rxjs'
+import { fromEvent } from 'rxjs'
+import {map} from 'rxjs/operators'
+const progressBar = <HTMLDivElement> document.querySelector('.progress-bar')
+
+// function que hará el calculo
+function getPercentageScroll(event:any):number{
+    const { scrollTop,scrollHeight,clientHeight } = event.target.documentElement
+    return (scrollTop / (scrollHeight - clientHeight))*100
+}
+
+console.log(progressBar);
+
+// streams
+const scroll$ = fromEvent(document,'scroll')
+
+const progress$ = scroll$.pipe(
+    map(getPercentageScroll)
+)
+
+progress$.subscribe(percentage=>{
+    progressBar.style.width = `${percentage}%` 
+})
