bf6fc3d889b528e2f6c056215a513aab70bc2d21
MaurickThom <thomtwd@gmail.com>
2020-03-19 23:25:40 -0500

    sample rxjsOperators

--- Rxjs-desde-cero-hasta-los-detalles/ts-webpack/webpack-workflow/src/ts/sample.ts
+++ Rxjs-desde-cero-hasta-los-detalles/ts-webpack/webpack-workflow/src/ts/sample.ts
@@ -1,4 +1,5 @@
 import { sample } from "rxjs/operators";
+import { interval, fromEvent } from "rxjs";
 
 // el sample funciona con dos observables
 /**
@@ -23,3 +24,10 @@ click$      ---e---------------e-------------e-------e---
 
             ---4---------------3---------------------3----
  */
+
+const interval$ = interval(500)
+const click$ = fromEvent<MouseEvent>(document,'click')
+
+interval$.pipe(
+    sample(click$) // obtengo la muestra de interval al momento de que haga click
+).subscribe(console.log)
