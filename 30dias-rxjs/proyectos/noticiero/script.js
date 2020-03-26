4c042f02b04d468640b8bedec48878fe52cc4642
MaurickThom <thomtwd@gmail.com>
2020-03-26 11:36:50 -0500

    final

--- 30dias-rxjs/proyectos/noticiero/script.js
+++ 30dias-rxjs/proyectos/noticiero/script.js
@@ -1,5 +1,13 @@
 const {Observable,Subject,operators,from,of,fromEvent,combineLatest} = rxjs
-const {  map,filter,scan,mapTo ,distinct,pluck,take} = rxjs.operators
+const {  map,filter,scan,mapTo ,distinct,pluck,take,repeat,withLatestFrom} = rxjs.operators
+
+
+// https://rxjs-dev.firebaseapp.com/api/operators/repeat
+// https://medium.com/@martinkonicek/rx-combinelatest-vs-withlatestfrom-ccd98cc1cd41
+// https://medium.com/ng-gotchas/combinelatest-vs-withlatestfrom-77bf4f01c64f
+// https://www.learnrxjs.io/learn-rxjs/operators/combination/withlatestfrom
+// https://rxjs-dev.firebaseapp.com/api/operators/withLatestFrom
+// a.withLatestFrom(b) a combina con el ultimo de b
 
 const _days = document.getElementById('weekDays')
 const day$ = fromEvent(_days,'click')
@@ -35,14 +43,23 @@ const deliveryMan$ = combineLatest(
 const thom = deliveryMan$.pipe(
     filter(order=>order.day==='Domingo' && order.newsPapers.find(
         newpaper=>newpaper === 'ABC'
-    )),take(1)
+    )),
+    take(1),
+    repeat()
 )
 
 
-const thomSub = () => {
-    thom.subscribe({
-        next : data => console.log(data),
-        complete: _ => thomSub()
-    })
-}
-thomSub()
+const erick = sendNews.pipe(
+    withLatestFrom(day$,newsPapers$),
+    map(([send,day,newsPapers])=>({send,day,newsPapers}))
+).subscribe(console.log)
+
+thom.subscribe(console.log)
+
+// const thomSub = () => {
+//     thom.subscribe({
+//         next : data => console.log(data),
+//         complete: _ => thomSub()
+//     })
+// }
+// thomSub()
