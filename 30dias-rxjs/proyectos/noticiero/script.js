65e3a81e87259c2fe099210717c663d761f5c792
MaurickThom <thomtwd@gmail.com>
2020-03-26 01:33:09 -0500

    .

--- 30dias-rxjs/proyectos/noticiero/script.js
+++ 30dias-rxjs/proyectos/noticiero/script.js
@@ -1,21 +1,21 @@
 const {Observable,Subject,operators,from,of,fromEvent,combineLatest} = rxjs
-const {  map,filter,scan,mapTo ,distinct} = rxjs.operators
+const {  map,filter,scan,mapTo ,distinct,pluck,take} = rxjs.operators
 
 const _days = document.getElementById('weekDays')
 const day$ = fromEvent(_days,'click')
                 .pipe(
-                    map(event=>event.target),
+                    pluck('target'),
                     filter(target=>target.nodeName==='BUTTON'),
-                    map(button=>button.innerText)
+                    pluck('innerText')
                 )
 
 const _newsPapers =  document.getElementById('newspapers')
 
 const newsPapers$ = fromEvent(_newsPapers,'click')
                 .pipe(
-                    map(event=>event.target),
+                    pluck('target'),
                     filter(target=>target.nodeName==='BUTTON'),
-                    map(button=>button.innerText),
+                    pluck('innerText'),
                     distinct(),
                     scan((acc,curr)=>acc.concat(curr),[])
                 )
@@ -30,4 +30,19 @@ const deliveryMan$ = combineLatest(
     day$,newsPapers$,sendNews
 ).pipe(
     map(([day,newsPapers,send])=>({day,newsPapers}))
-).subscribe(console.log)
+)
+
+const thom = deliveryMan$.pipe(
+    filter(order=>order.day==='Domingo' && order.newsPapers.find(
+        newpaper=>newpaper === 'ABC'
+    )),take(1)
+)
+
+
+const thomSub = () => {
+    thom.subscribe({
+        next : data => console.log(data),
+        complete: _ => thomSub()
+    })
+}
+thomSub()
